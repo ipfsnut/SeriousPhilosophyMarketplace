88a535e068dbbf57e8453f2ca8d3d080580d7af4
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-05 00:20:50 -0600

    attempting to switch to Optimism from Goerli

--- pages/listing/[listingId].tsx
+++ pages/listing/[listingId].tsx
@@ -63,7 +63,7 @@ const ListingPage: NextPage = () => {
         await marketplace?.direct.makeOffer(
           listingId, // The listingId of the listing we want to make an offer for
           1, // Quantity = 1
-          NATIVE_TOKENS[ChainId.Goerli].wrapped.address, // Wrapped Ether address on Goerli
+          NATIVE_TOKENS[ChainId.Optimism].wrapped.address, // Wrapped Ether address on Optimism
           bidAmount // The offer amount the user entered
         );
       }
@@ -88,7 +88,7 @@ const ListingPage: NextPage = () => {
     try {
       // Ensure user is on the correct network
       if (networkMismatch) {
-        switchNetwork && switchNetwork(ChainId.Goerli);
+        switchNetwork && switchNetwork(ChainId.Optimism);
         return;
       }
 
