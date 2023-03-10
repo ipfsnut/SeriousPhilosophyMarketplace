af6d76dd1aacead0166483fa807c6e3ce6327c7f
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-10 00:49:54 -0600

    Update _app.tsx
    updated description to Serious Philosophy info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,15 +11,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb Marketplace with Next.JS</title>
+        <title>Serious Philosophy Optimism Marketplace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+          content="Buy, Auction, and Sell Serious Philosophy NFTs"
         />
         <meta
           name="keywords"
-          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+          content="Serious Philosophy, Marketplace, NFT Marketplace"
         />
       </Head>
       <Header />
